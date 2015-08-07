3d0be6a4495236f5d7205195fb936059554e21a1
Yohahn Kim <yohahn.kim@gmail.com>
2015-08-06 20:37:39 -0400

    3-2 / template / setup a domain helper on the postItem

---
+++ client/template/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yohahn on 8/6/15.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
