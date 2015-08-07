4d9e79a4f7d9123d74af3c4fbb97c92a045d4a52
Yohahn Kim <yohahn.kim@gmail.com>
2015-08-06 20:44:42 -0400

    4-3 / collections / wire collection into postsList template

--- client/template/posts/posts_list.js
+++ client/template/posts/posts_list.js
@@ -1,20 +1,8 @@
 /**
  * Created by yohahn on 8/6/15.
  */
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
