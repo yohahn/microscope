f582de07dd689a59a98347a12a9bf7f69311e518
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-02 16:57:24 -0400

    8-1 / editing post / add edit post form

--- client/template/posts/post_item.js
+++ client/template/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by yohahn on 8/6/15.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
