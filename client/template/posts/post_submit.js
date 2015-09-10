48b16f6f343389d36f9771b2a712ad96e4b5f755
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-10 19:45:15 -0400

    9-2 / errors / actually use the error reporting

--- client/template/posts/post_submit.js
+++ client/template/posts/post_submit.js
@@ -13,8 +13,14 @@ Template.postSubmit.events({
         Meteor.call('postInsert', post, function(error, result) {
             // display the error to the user and abort
             if (error) {
-                return alert(error.reason);
+                return throwError(error.reason);
             }
+
+            // show this result but route anyway
+            if (result.postExists) {
+                throwError('This link has already been posted');
+            }
+
             Router.go('postPage', {_id: result._id});
         });
     }
