e52c02fe7e2706fc2319d2ef139723eebe9bb20d
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-10 20:10:35 -0400

    9-4 / error / validate post contents on submission

--- client/template/posts/post_submit.js
+++ client/template/posts/post_submit.js
@@ -1,6 +1,19 @@
 /**
  * Created by yohahn on 9/1/15.
  */
+Template.postSubmit.onCreated(function() {
+    Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -9,6 +22,11 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
+
+        var errors = validatePost(post);
+        if (errors.title || errors.url) {
+            return Session.set('postSubmitErrors', errors);
+        }
 
         Meteor.call('postInsert', post, function(error, result) {
             // display the error to the user and abort
