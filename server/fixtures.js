646fd567371130072911671194dedc7f778e88c7
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-22 20:19:21 -0400

    10-5 / comments / denormalized the number of comments into the post

--- server/fixtures.js
+++ server/fixtures.js
@@ -19,7 +19,8 @@ if (Posts.find().count() === 0) {
         userId: sacha._id,
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
-        submitted: new Date(now - 7 * 3600 * 1000)
+        submitted: new Date(now - 7 * 3600 * 1000),
+        commentsCount: 2
     });
 
     Comments.insert({
@@ -43,7 +44,8 @@ if (Posts.find().count() === 0) {
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://meteor.com',
-        submitted: new Date(now - 10 * 3600 * 1000)
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0
     });
 
     Posts.insert({
@@ -51,6 +53,7 @@ if (Posts.find().count() === 0) {
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
-        submitted: new Date(now - 12 * 3600 * 1000)
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0
     });
 }
