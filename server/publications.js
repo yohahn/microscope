5ac0903491718b1ce337f8b92f81f79d545a944d
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-22 20:11:23 -0400

    10-4 / comments / make a simple publication/subscription for comments

--- server/publications.js
+++ server/publications.js
@@ -5,6 +5,7 @@ Meteor.publish('posts', function() {
     return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-    return Comments.find();
+Meteor.publish('comments', function(postId) {
+    check(postId, String);
+    return Comments.find({postId: postId});
 });
