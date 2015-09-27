6bce0eb0ed45bd69f60b52ae8065460e8c7e3fdd
Yohahn Kim <yohahn.kim@gmail.com>
2015-09-27 16:50:54 -0400

    13-3 / voting / add pluralize helper to format better

---
+++ client/helpers/spacebars.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yohahn on 9/27/15.
+ */
+Template.registerHelper('pluralize', function(n, thing) {
+    // fairly stupid spluralizer
+    if (n === 1) {
+        return '1 ' + thing;
+    } else {
+        return n + ' ' + thing + 's';
+    }
+});
